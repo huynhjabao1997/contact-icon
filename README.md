812e3081e42b4e22d63f75283a40c66629a4fd57
Alexander Huynh <>
2022-11-19 23:42:50 +0700

    Update comment and readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # IconContactMacSG
-Show the contact icon in the right bottom of the screen using iFrame
+Show the contact icon in the right bottom of the screen
+
+And then use iFrame to embed the HTML page
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = function prepareFrame() {
     let ifrm = document.createElement("iframe");
-    ifrm.setAttribute("src", "http://127.0.0.1:5500/home.html");
+    ifrm.setAttribute("src", "http://127.0.0.1:5500/home.html"); // Update this
     ifrm.style = "border: none; position: fixed; right: 30px; bottom: 30px; z-index:999; width: 100%; height: 100%;";
     document.body.append(ifrm);
 }
